b5ca864584c79f98113f85583e37eaacabc7dcfc
Bharat Pawar <bharat1234512345777@gmail.com>
2026-02-26 20:26:46 +0530

    da

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,9 +33,11 @@ const Contact = () => {
     setLoading(true);
 
     emailjs
-      .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      .send( PP_EMAILJS_TEMPLATE_ID,
+ 
+        "service_0bz97sb",
+        "template_34yndq9",
+ 
         {
           from_name: form.name,
           to_name: "WylerChain",
